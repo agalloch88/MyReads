3c7c69f7cd163c1d546360bf3ad2711596d74b50
Ryan Kirsch <agalloch88@gmail.com>
2022-06-22 17:47:24 -0400

    modularize shelves, shelf, books to show static data from books array

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-function Book() {
+function Book({book}) {
   return (
     <div className="book">
       <div className="book-top">
@@ -10,7 +10,7 @@ function Book() {
             width: 128,
             height: 193,
             backgroundImage:
-              'url("http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api")',
+              `${book.cover.backgroundImage}`,
           }}
         ></div>
         <div className="book-shelf-changer">
@@ -25,8 +25,8 @@ function Book() {
           </select>
         </div>
       </div>
-      <div className="book-title">To Kill a Mockingbird</div>
-      <div className="book-authors">Harper Lee</div>
+      <div className="book-title">{book.title}</div>
+      <div className="book-authors">{book.author}</div>
     </div>
   );
 }
