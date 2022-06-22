8a0679e2184cd6ba04e713fe8233e1794099aef8
Ryan Kirsch <agalloch88@gmail.com>
2022-06-22 17:51:07 -0400

    update shelf title based on props

--- src/components/Shelf.js
+++ src/components/Shelf.js
@@ -1,14 +1,14 @@
 import React from "react";
 import Book from "./Book";
 
-function Shelf({ books }) {
+function Shelf({ books, title }) {
   return (
     <div className="bookshelf">
-      <h2 className="bookshelf-title">Currently Reading</h2>
+      <h2 className="bookshelf-title">{title}</h2>
       <div className="bookshelf-books">
         <ol className="books-grid">
           {books.map((book) => (
-            <li>
+            <li key={book.name}>
               <Book book={book} />
             </li>
           ))}
