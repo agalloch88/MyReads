c51eee67a7be6e6250ec72b599b659c00b8e0fd0
Ryan Kirsch <agalloch88@gmail.com>
2022-06-24 08:15:27 -0400

    adjust shelf options

--- src/components/Book.js
+++ src/components/Book.js
@@ -21,7 +21,6 @@ function Book({book, changeShelf}) {
             <option value="currentlyReading">Currently Reading</option>
             <option value="wantToRead">Want to Read</option>
             <option value="read">Read</option>
-            <option value="none">None</option>
           </select>
         </div>
       </div>
